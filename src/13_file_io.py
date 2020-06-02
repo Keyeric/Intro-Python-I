cb1908c1aff2bc5bbcc9ba57ba1114242a4778f3
Keyeric <keyeric.bristol@gmail.com>
2020-06-02 14:58:05 -0700

    Im not writing a fucking calender

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,11 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-with open('foo') as f:
+with open('./src/foo.txt') as f:
     read_data = f.read()
-
-# We can check that the file has been automatically closed.
-f.closed
+print(read_data)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -22,3 +20,12 @@ f.closed
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+string1 = "Open up a file called 'bar.txt' (which doesn't exist yet) for writing."
+string2 = "Write three lines of arbitrary content to that file, then close the file."
+string3 = "Open up 'bar.txt' and inspect it to make sure that it contains what you expect it to contain"
+L = [string1, string2, string3]
+
+with open(r'./src/bar.txt', "w+") as f:
+    f.writelines(L)
+    read_lines = f.read()
+print(read_lines)

a89331c956756856b871e1bfa3db6b02bd7a3cc1
Keyeric <keyeric.bristol@gmail.com>
2020-06-04 12:28:33 -0700

    Classes completion

--- src/15_classes.py
+++ src/15_classes.py
@@ -2,28 +2,56 @@
 # constructor
 
 # YOUR CODE HERE
+class Latlon:
+    '''Coordinate class'''
+    def __init__(self,lat,lon):
+        self.lat = lat
+        self.lon = lon
 
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
+class Waypoint(Latlon):
+    '''inherit from Latlon'''
+    def __init__(self, name, lat, lon):
+        super().__init__(lat, lon)
+        self.name = name
+
+    def __str__(self):
+        return f'This waypoint is called {self.name}, The Latitude and Longitude are as follows: {self.lat}, {self.lon}'
+    
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
+class Geocache(Waypoint):
+    def __init__(self, name, difficulty, size, lat, lon):
+        super().__init__(name, lat, lon)
+        self.size = size
+        self.difficulty = difficulty
+
+    def __str__(self):
+        return f'This geocache is called {self.name}, the difficulty level is {self.difficulty}, the size is {self.size}, and the Latitude and Longitude are as follows: {self.lat}, {self.lon}'
+
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
+newWaypoint = Waypoint("Catacombs", 41.70505, -121.51521)
+print(newWaypoint.name, newWaypoint.lat, newWaypoint.lon)
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
-print(waypoint)
+print(newWaypoint.__str__())
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
+newGeocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
+print(newGeocache.name, newGeocache.difficulty, newGeocache.size, newGeocache.lat, newGeocache.lon)
+
 
 # Print it--also make this print more nicely
-print(geocache)
+print(newGeocache.__str__())
